3926300818a505dddf321ce76a51e064727d9124
ffonseca <ffonseca1985@gmail.com>
2015-12-02 21:16:19 -0200

    Adcionado Application

---
+++ src/Application/Contracts/Core/ApplicationBase.cs
@@ -0,0 +1,23 @@
+﻿using LGroup.Arquitetura.DDD.Domain.Contracts.UnityOfWork;
+using Microsoft.Practices.ServiceLocation;
+
+namespace LGroup.Arquitetura.DDD.Application.Contracts.Core
+{
+    public abstract class ApplicationBase
+    {
+        private IUnityOfWork _unityOfWork;
+
+        // Instanciar o PessoaContexto 
+        public void Begin()
+        {
+            _unityOfWork = ServiceLocator.Current.GetInstance<IUnityOfWork>();
+            _unityOfWork.Begin();
+        }
+
+        // Salvando (Commit)
+        public void SaveChanges()
+        {
+            _unityOfWork.SaveChanges();
+        }
+    }
+}
---
+++ src/Application/Services/PessoaApplicationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LGroup.Arquitetura.DDD.Application.Contracts.Core;
+using LGroup.Arquitetura.DDD.Application.Contracts.Services;
+using LGroup.Arquitetura.DDD.Domain.Contracts.Repositories;
+using LGroup.Arquitetura.DDD.Domain.Entities;
+
+namespace LGroup.Arquitetura.DDD.Application.Services
+{
+    public class PessoaApplicationService : ApplicationBase, IPessoaApplicationService
+    {
+        private readonly IPessoaRepository _pessoaRepository;
+
+        public PessoaApplicationService(IPessoaRepository pessoaRepository)
+        {
+            _pessoaRepository = pessoaRepository;
+        }
+
+        public void Add(PessoaEntity pessoa)
+        {
+            Begin();
+            // Adicionar Usuário
+            // Mandar Email
+            // Gravar Log
+            _pessoaRepository.Add(pessoa);
+            SaveChanges();
+        }
+    }
+}
